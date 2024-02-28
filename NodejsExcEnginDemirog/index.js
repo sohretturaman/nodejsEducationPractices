e665740ca5e24ee7d5d8e9f26d7700e349dbf0b5
şöhrett <sohretturaman@gmail.com>
2024-02-28 12:55:16 +0300

    deleted unnecessary node modules

--- NodejsExcEnginDemirog/index.js
+++ NodejsExcEnginDemirog/index.js
@@ -8,9 +8,27 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello from express intial page");
+  res.send("hello from express server");
 });
 
-app.listen(5000, (req, res) => {
+app.get("/admin", (req, res) => {
+  res.send("hello from admin page get func");
+});
+app.get("/about", (req, res) => {
+  res.send("got get request in about page");
+});
+
+app.put("/admin", (req, res) => {
+  res.send("got put request in admin page");
+});
+app.delete("/about", (req, res) => {
+  res.send("got delete request in about page");
+});
+
+app.post("/admin", (req, res) => {
+  res.send("got post request in root page");
+});
+
+const server = app.listen(5000, (req, res) => {
   console.log("server in port 5000");
 });
